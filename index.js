ad7f457b2f46e972ced442f55d4728a0acc70a4d
Jorge Caloir <47183518+Jorgecaloir1992@users.noreply.github.com>
2023-11-12 18:56:34 -0300

    se añade una consulta SQL

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const routerApi = require('./router') //aqui estamos llamando el modulo de rutas
+const mysql = require('mysql2');
 
 const app = express();
 const port = 3000;
+
+// Creando la conexión a la base de datos
+const connection = mysql.createConnection({
+  host: '34.22.222.92',
+  user: 'root',
+  password: 'Yoma2024**oz',
+  database: 'test-database'
+});
+
+connection.connect((error) => {
+  if (error) {
+    console.error('Error al conectar a la base de datos:', error);
+    return;
+  }
+  console.log('Conexión exitosa a la base de datos');
+});
 
 app.use(express.json());      // con este ajustes ya puedes recibir informacion de tipo json en el insomia
 
